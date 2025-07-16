8844fd6b4afd8acfc60dc5537406b92f8248ebfa
EliasOMacias <167808478+EliasOMacias@users.noreply.github.com>
2025-07-16 10:45:06 -0700

    Commit prior to updating script to push to render hosted DB

---
+++ scripts/reddit_post_db_updater.py
@@ -0,0 +1,75 @@
+import psycopg
+import praw
+from datetime import datetime
+
+with psycopg.connect(
+    dbname="silverbugs",
+    user="silver_bug",
+    password="Th1s1s4p0stgresql",
+    host="localhost",  # or your cloud host
+    port=5432
+) as conn:
+
+    with conn.cursor() as cur:
+        cur.execute("SELECT MAX(created_utc) FROM post_data;")
+        last_timestamp = cur.fetchone()[0].timestamp()
+
+reddit_creds = praw.Reddit(client_id='GemgQF90kR7V1F75su4t7g',
+                     client_secret = 'IAePy0shvSac6ZY_GUXSkqBLp-AQsw',
+                     username='its_lit_in_here_huh',
+                     password='chillpassword420',
+                     user_agent='pms scraping bot by /u/its_lit_in_here_bot (academic use)')
+
+subreddit = reddit_creds.subreddit('Pmsforsale')
+
+new_posts = []
+for post in subreddit.new(limit=999):
+    if post.created_utc > last_timestamp-30000:
+        new_posts.append({
+            'id': post.id,
+            'title': post.title,
+            'created_utc': post.created_utc,
+            'score': post.score,
+            'num_comments': post.num_comments,
+            'url': post.url,
+            'selftext': post.selftext
+        })
+
+print(f"Collected {len(new_posts)} posts")
+
+
+for post in new_posts:
+    post["created_utc"] = datetime.utcfromtimestamp(post["created_utc"])
+
+
+with psycopg.connect(
+    dbname="silverbugs",
+    user="silver_bug",
+    password="Th1s1s4p0stgresql",
+    host="localhost", 
+    port=5432
+) as conn:
+
+
+    with conn.cursor() as cur:
+        insert_query = """
+        INSERT INTO post_data (id, url, title, selftext, created_utc, num_comments, score)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        ON CONFLICT (id) DO NOTHING;
+        """
+    
+        for post in new_posts:
+            cur.execute(
+                insert_query,
+                (
+                    post["id"],
+                    post["url"],
+                    post["title"],
+                    post["selftext"],
+                    post["created_utc"],
+                    post["num_comments"],
+                    post["score"]
+                )
+            )
+        conn.commit()
+
